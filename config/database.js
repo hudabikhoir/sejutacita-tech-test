a0e4500d80c7a56e89855ab287d5af1f2ec4dac1
hudabikhoir <nurhuda@alterra.id>
2021-06-22 19:51:19 +0700

    (feat-webserver): docker
    containerize app with docker

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 //Set up mongoose connection
 const mongoose = require('mongoose');
-const mongoDB = 'mongodb://localhost/sejutacita';
+const mongoDB = 'mongodb://mongodbs:27017/sejuta-cita';
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = global.Promise;
 module.exports = mongoose;
